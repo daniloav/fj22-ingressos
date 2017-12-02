f0b81b2d5640f04e38ef1289b786d4e97911b62b
Danilo Oliveira <danilo.av@gmail.com>
2017-12-02 15:47:49 -0200

    Criando funcionalidade de Sessao

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,53 @@
+package br.com.caelum.ingresso.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.form.SessaoForm;
+
+@Controller
+public class SessaoController {
+	@Autowired
+	private SalaDao salaDao;
+	@Autowired
+	private FilmeDao filmeDao;
+	@Autowired
+	private SessaoDao sessaoDao;
+
+	@Transactional
+	@PostMapping(value = "/admin/sessao")
+	public ModelAndView salva(@Valid SessaoForm sessaoForm, BindingResult result) {
+		if (result.hasErrors()) {
+			return form(sessaoForm.getSalaId(), sessaoForm);
+		}
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/" + sessaoForm.getSalaId() + "/sessoes");
+
+		Sessao sessao = sessaoForm.toSessao(salaDao, filmeDao);
+		sessaoDao.save(sessao);
+
+		return modelAndView;
+	}
+
+	@GetMapping("/admin/sessao")
+	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+
+		modelAndView.addObject("sala", salaDao.findOne(salaId));
+		modelAndView.addObject("filmes", filmeDao.findAll());
+		modelAndView.addObject("form", form);
+		return modelAndView;
+	}
+
+}
